279d4bbcd6c345b050f5b8ee49f08a0fda1e9557
margheritabenigni <margherita.benigni@campusdavinci.edu.it>
2025-10-14 11:38:46 +0200

    aggiunta dell'ascissa e dell'ordinata massima

--- es 4 marghe.py
+++ es 4 marghe.py
@@ -2,7 +2,7 @@
 #random compresi nell'intervallo 0,10 . calcolare il
 #punto con ascissaminima e coordinata minima.
 
-import:random
+import random
 
 ascissa(0)
 ordinata(0)
@@ -14,16 +14,22 @@ punti:[]
 
 for i in range(0,20):
    x.append(indice)(random.randint(0,10))
-   y.append(indice(random.randint(0,10))
-            
+   y.append(indice(random.randint(0,10)))
+#ascissa massima ?
+#scorrere la lista delle x , trova il massimo e salvare l'indice
+#visualizzare poi con una print il numero
 
+for x in range(0,20):
+    if x[i]>massimo:
+ print (massimo)
+# ho aggiunto la ricerca del punto con ascissa massima 
 punti_cartesiano=[]
 for i in range(0,20):
-    punto = (random.randint(0,10),random.randint(0,10)
+    punto = (random.randint(0,10),random.randint(0,10))
     punti_cartesiano.append(punto)
              
 #come accedere alla x del primo punto
 print(punti_cartesiano[0][0])
 
 #come accedere alla y del primo punto
-             print(punti_cartesiano[0][1])
+print(punti_cartesiano[0][1])
