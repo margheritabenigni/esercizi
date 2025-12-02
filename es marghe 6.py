72f1b3c5f43f3c96704234aacae660f21dccb532
margheritabenigni <margherita.benigni@campusdavinci.edu.it>
2025-12-02 11:34:22 +0100

    add esercizio funzioni sottoproblemi

---
+++ es marghe 6.py
@@ -0,0 +1,23 @@
+#riscrivere una funzione che dati due numeri interni restituisca il maggiore
+
+def nMaggiore (numero1,numero2):
+    if numero1>numero2:
+        return numero1
+    else:
+        return numero2
+#scrivere una procedura che dato un numero intero stampi a video
+#se è pari o dispari
+    def nPariDispari (numero1)
+    ifnumero1%2 == 0
+        print("il numero è pari")
+    else:
+        print("il numero è dispari")
+        
+numero1=input("inserire il primo numero")
+numero1=int(numero1)
+numero2=input("inserire il secondo numero")
+numero2=int(numero2)
+
+risultato = nMaggiore(numero1,numero2)
+print(risultato)
+# con questo esercizio ho creato un programma che dati due interi stampia video se il maggiore sia pari o dispari
